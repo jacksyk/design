b6360fbc8de58696aa947abb1e69f44ba55c289f
舒毅康 <185901261@qq.com>
2025-02-03 11:12:34 +0800

    feat: 加入vconsole调试面板

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,11 @@ import 'animate.css';
 import './global.css';
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
 // 更多信息见文档：https://umijs.org/docs/api/runtime-config#getinitialstate
+
+import VConsole from 'vconsole';
+
+new VConsole();
+
 export async function getInitialState(): Promise<{ name: string }> {
   return { name: '@umijs/max' };
 }
